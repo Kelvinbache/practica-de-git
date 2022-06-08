328458b9ab3fda4c0d1c70153f860aded7ff207a
kelvin abache <kelvinabache12@gmail.com>
2022-06-08 17:28:46 -0430

    actualizando archivo

---
+++ prcticar.js
@@ -0,0 +1,28 @@
+document.write(nombre);
+
+let nombre = " ";
+let apellido = "";
+nombre = kelvin;
+
+
+/*tips para no mostra toda informcion de los estados
+
+1) git status -s (sirve para mostra el estaado del contenido)
+
+*/
+
+
+/*comndos para ver los cambios en vivvo
+ 1) git diff (permite ver los cambios)
+ 2) git diff --staged (vemos los archivos que estn)
+ */
+
+ /*como ver los commit 
+ 1) git log (muestra toda l informacion)
+ 2) git log --oneline (muestraa solo los commit que has ello)
+ */
+
+ /*forma para ver que rama estas  
+ git branch (te permite ver que rama estas)
+ 
+ */
